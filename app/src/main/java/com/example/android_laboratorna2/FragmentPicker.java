edf74fe73f6e17757fa4cb98f8ec457b6adf4cf1
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-04-07 22:30:39 +0300

    add method onAttach in FragmentPicker java class

--- app/src/main/java/com/example/android_laboratorna2/FragmentPicker.java
+++ app/src/main/java/com/example/android_laboratorna2/FragmentPicker.java
@@ -1,4 +1,5 @@
 package com.example.android_laboratorna2;
+import android.content.Context;
 import android.widget.Button;
 import android.widget.RadioGroup;
 
@@ -15,6 +16,17 @@ public class FragmentPicker extends Fragment {
     private RadioGroup radioGroupPriceRange;
     private Button buttonOk;
 
+    private OnFragmentSendDataListener fragmentSendDataListener;
 
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            fragmentSendDataListener = (OnFragmentSendDataListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must release interface OnFragmentInteractionListener");
+        }
+    }
 }
