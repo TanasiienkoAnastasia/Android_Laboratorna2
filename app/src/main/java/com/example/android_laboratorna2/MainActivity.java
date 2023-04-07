4fae353fa7f9a14d9c7b7f79e56ae233b82d2bba
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-04-07 23:18:00 +0300

    add method OcCreate in MainActitvity java class

--- app/src/main/java/com/example/android_laboratorna2/MainActivity.java
+++ app/src/main/java/com/example/android_laboratorna2/MainActivity.java
@@ -8,5 +8,15 @@ public class MainActivity extends AppCompatActivity {
     private FragmentPicker fragmentPicker;
     private FragmentOutput fragmentOutput;
 
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentPicker = (FragmentPicker) getSupportFragmentManager()
+                .findFragmentById(R.id.frameLayoutPicker);
+
+        fragmentOutput = (FragmentOutput) getSupportFragmentManager()
+                .findFragmentById(R.id.frameLayoutOutput);
+    }
 }
