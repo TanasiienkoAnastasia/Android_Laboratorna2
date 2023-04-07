d50a36100780524215a594c383fb5627271b8934
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-04-07 22:44:27 +0300

    add OnCreateView() and getTextCheckedRadioButton() methods

--- app/src/main/java/com/example/android_laboratorna2/FragmentPicker.java
+++ app/src/main/java/com/example/android_laboratorna2/FragmentPicker.java
@@ -1,6 +1,11 @@
 package com.example.android_laboratorna2;
 import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import androidx.constraintlayout.widget.Group;
@@ -12,8 +17,8 @@ public class FragmentPicker extends Fragment {
         void onSendData(String dataType, String dateFirm);
     }
 
-    private RadioGroup radioGroupColour;
-    private RadioGroup radioGroupPriceRange;
+    private RadioGroup RadioGroupColour;
+    private RadioGroup RadioGroupPriceRange;
     private Button buttonOk;
 
     private OnFragmentSendDataListener fragmentSendDataListener;
@@ -28,5 +33,39 @@ public class FragmentPicker extends Fragment {
             throw new ClassCastException(context.toString()
                     + " must release interface OnFragmentInteractionListener");
         }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_picker, container, false);
+
+        RadioGroupColour = view.findViewById(R.id.RadioGroupColour);
+        RadioGroupPriceRange = view.findViewById(R.id.RadioGroupPriceRange);
+        buttonOk = view.findViewById(R.id.buttonOk);
+
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String type = getTextCheckedRadioButton(RadioGroupColour);
+                String firm = getTextCheckedRadioButton(RadioGroupPriceRange);
+                fragmentSendDataListener.onSendData(type, firm);
+            }
+        });
+
+        return view;
+    }
+
+    private String getTextCheckedRadioButton(RadioGroup radioGroup){
+        String text = "";
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            RadioButton radioButton = (RadioButton) radioGroup.getChildAt(i);
+            if (radioButton.isChecked())
+            {
+                text = radioButton.getText().toString();
+            }
+        }
+        return text;
     }
 }
