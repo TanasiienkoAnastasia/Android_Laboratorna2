829d4d9909ce0418a6793f50d2d1b58e9b0d90cf
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-04-07 23:16:48 +0300

    add fragmentPicker and FragmantOutput

--- app/src/main/java/com/example/android_laboratorna2/MainActivity.java
+++ app/src/main/java/com/example/android_laboratorna2/MainActivity.java
@@ -5,10 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentPicker fragmentPicker;
+    private FragmentOutput fragmentOutput;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    
 }
