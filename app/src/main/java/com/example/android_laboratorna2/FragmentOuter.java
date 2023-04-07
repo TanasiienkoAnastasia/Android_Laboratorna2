da52b344f45ea358d0c90e9d37180e0dbde0bdef
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-04-07 19:45:18 +0300

    add tools:context=".FragmentOutput" in fragment_output.xml

--- app/src/main/java/com/example/android_laboratorna2/FragmentOuter.java
+++
@@ -1,9 +0,0 @@
-package com.example.android_laboratorna2;
-
-import androidx.fragment.app.Fragment;
-
-public class FragmentOuter extends Fragment {
-    public FragmentOuter() {
-        super(R.layout.fragment_outer);
-    }
-}
---
+++ app/src/main/java/com/example/android_laboratorna2/FragmentOutput.java
@@ -0,0 +1,9 @@
+package com.example.android_laboratorna2;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentOutput extends Fragment {
+    public FragmentOutput() {
+        super(R.layout.fragment_output);
+    }
+}
