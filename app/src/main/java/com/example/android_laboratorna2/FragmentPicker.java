86471d2d6f707b7847336553bcb71cef5dde4d72
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-04-07 22:20:52 +0300

    add RadioGroups and Button in FragmentPicker java class

--- app/src/main/java/com/example/android_laboratorna2/FragmentPicker.java
+++ app/src/main/java/com/example/android_laboratorna2/FragmentPicker.java
@@ -1,4 +1,8 @@
 package com.example.android_laboratorna2;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+import androidx.constraintlayout.widget.Group;
 import androidx.fragment.app.Fragment;
 
 
@@ -6,4 +10,10 @@ public class FragmentPicker extends Fragment {
     public FragmentPicker() {
         super(R.layout.fragment_picker);
     }
+
+
+    private RadioGroup radioGroupColour;
+    private RadioGroup radioGroupPriceRange;
+    private Button buttonOk;
+
 }
