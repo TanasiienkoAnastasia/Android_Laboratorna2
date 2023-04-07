3c30a3f53f23794b8ad725b6b0fcb04d5d0f072c
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-04-07 23:19:05 +0300

    add method CreateAlertDialog in MainActivity java class

--- app/src/main/java/com/example/android_laboratorna2/MainActivity.java
+++ app/src/main/java/com/example/android_laboratorna2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android_laboratorna2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentOutput = (FragmentOutput) getSupportFragmentManager()
                 .findFragmentById(R.id.frameLayoutOutput);
+    }
+
+    private void CreateAlertDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setMessage(message);
+        builder.setTitle("Warning!");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Ok", (DialogInterface.OnClickListener) (dialog, which) -> {
+            dialog.cancel();
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
